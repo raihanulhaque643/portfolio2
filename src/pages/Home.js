544549e834db550f02c1c2970f66108bcb96f612
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-16 20:00:30 +0600

    typewriter effect added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import me from '../assets/me.jpg'
+import Typewriter from 'typewriter-effect'
 
 const Home = () => {
     return (
         <div className="flex flex-row min-w-screen max-w-auto justify-center align-center min-h-screen max-height-auto bg-black">
             <div className="flex flex-col w-full items-center justify-center bg-green-200">
-                <div className="flex flex-col text-black text-4xl sm:text-4xl md:text-6xl lg:text-8xl font-bold px-2">
-                <span className="my-2">Hi.</span>
-                <span className="my-2">I'm Raihanul.</span>
-                <span className="my-2">I build stuff with Javascript.</span>
+                <div className="flex flex-col text-black text-4xl sm:text-4xl md:text-6xl lg:text-8xl font-bold px-2 my-2">
+                    <Typewriter 
+                    onInit={(typewriter) => {
+                        typewriter.typeString('Hi!')
+                        .pauseFor(2000)
+                        // .deleteAll()
+                        .typeString('<br>I am Raihanul.')
+                        .pauseFor(2000)
+                        // .deleteAll()
+                        .typeString('<br>I build stuff with Javascirpt!')
+                        .start()
+                    }}
+                    />
+                {/* <span>Hi.</span>
+                <span>I'm Raihanul.</span>
+                <span>I build stuff with Javascript.</span> */}
                 </div>
             </div>
             <div className="flex flex-col w-full items-center justify-center">
