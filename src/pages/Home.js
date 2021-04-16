44eec5c5702e2b75c95d84004cdda97ec58748eb
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-16 19:04:32 +0600

    navbar styles updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import me from '../assets/me.jpg'
 
 const Home = () => {
     return (
         <div className="flex flex-row min-w-screen max-w-auto justify-center align-center min-h-screen max-height-auto">
-            <div className="flex flex-col w-full border items-center justify-center bg-green-200">
-                I am Web
+            <div className="flex flex-col w-full items-center justify-center bg-green-200">
+                <div className="flex flex-col text-green-800 text-4xl sm:text-4xl md:text-6xl lg:text-8xl font-bold px-2">
+                <span className="my-2">Hi.</span>
+                <span className="my-2">I'm Raihanul.</span>
+                <span className="my-2">I build stuff with Javascript.</span>
+                </div>
             </div>
-            <div className="flex flex-col w-full border items-center justify-center">
-                I am picture
+            <div className="flex flex-col w-full items-center justify-center bg-black">
+                <img className="rounded-full" src={me} alt="Pic not found"/>
             </div>
         </div>
     )
