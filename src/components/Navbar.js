88f272e5f416c4a333649a18ec81e5516d0b50b5
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-16 17:33:26 +0600

    navbar width changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 export default function Navbar({ setToken, fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <nav className="bg-gray-800">
-  <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <nav className="bg-green-800">
+  <div className="mx-auto px-2 sm:px-6 lg:px-8">
     <div className="relative flex items-center justify-between h-16">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
         {/* <!-- Mobile menu button--> */}
-        <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={() => {setNavbarOpen(!navbarOpen)}}>
+        <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={() => {setNavbarOpen(!navbarOpen)}}>
           <span className="sr-only">Open main menu</span>
           <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -28,11 +28,11 @@ export default function Navbar({ setToken, fixed }) {
         </div>
         <div className="hidden sm:block sm:ml-6">
           <div className="flex space-x-4">
-            <Link to="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</Link>
+            <Link to="/about" className="text-gray-300 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</Link>
 
-            <Link to="/portfolio" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Portfolio</Link>
+            <Link to="/portfolio" className="text-gray-300 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Portfolio</Link>
 
-            <Link to="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</Link>
+            <Link to="/contact" className="text-gray-300 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</Link>
           </div>
         </div>
       </div>
@@ -45,11 +45,11 @@ export default function Navbar({ setToken, fixed }) {
 
     <div class="sm:hidden" id="mobile-menu">
     <div class="px-2 pt-2 pb-3 space-y-1">
-      <Link to="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">About</Link>
+      <Link to="/about" className="text-gray-300 hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">About</Link>
 
-      <Link to="/portfolio" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Portfolio</Link>
+      <Link to="/portfolio" className="text-gray-300 hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Portfolio</Link>
 
-      <Link to="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</Link>
+      <Link to="/contact" className="text-gray-300 hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</Link>
     </div>
   </div>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            Home page
+        <div className="flex flex-row min-w-screen max-w-auto justify-center align-center min-h-screen max-height-auto">
+            <div className="flex flex-col w-full border items-center justify-center bg-green-200">
+                I am Web
+            </div>
+            <div className="flex flex-col w-full border items-center justify-center">
+                I am picture
+            </div>
         </div>
     )
 }
