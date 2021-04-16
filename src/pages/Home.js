97df30e987ebcdd12b97b74eb3a8c3b1decd9e71
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-16 19:12:02 +0600

    google font Playfair Display added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import me from '../assets/me.jpg'
 
 const Home = () => {
     return (
-        <div className="flex flex-row min-w-screen max-w-auto justify-center align-center min-h-screen max-height-auto">
+        <div className="flex flex-row min-w-screen max-w-auto justify-center align-center min-h-screen max-height-auto bg-black">
             <div className="flex flex-col w-full items-center justify-center bg-green-200">
                 <div className="flex flex-col text-green-800 text-4xl sm:text-4xl md:text-6xl lg:text-8xl font-bold px-2">
                 <span className="my-2">Hi.</span>
@@ -11,7 +11,7 @@ const Home = () => {
                 <span className="my-2">I build stuff with Javascript.</span>
                 </div>
             </div>
-            <div className="flex flex-col w-full items-center justify-center bg-black">
+            <div className="flex flex-col w-full items-center justify-center">
                 <img className="rounded-full" src={me} alt="Pic not found"/>
             </div>
         </div>
