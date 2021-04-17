8c644a1cdf64dccf63920683fc8a9a8c4375cf45
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-17 20:07:44 +0600

    some classes added to project/s

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,15 +2,30 @@ import React from 'react'
 
 const Project = ({ name,  image, order, repoFrontend, repoBackend, email, password, url}) => {
     return (
-        <div className="flex-col">
-            <div className="">{name}</div>
-            <div className="">{image}</div>
-            <div className="">{order}</div>
-            <div className="">{repoFrontend}</div>
-            <div className="">{repoBackend}</div>
-            <div className="">{email}</div>
-            <div className="">{password}</div>
-            <div className="">{url}</div>
+        <div className="flex-col min-h-96 max-h-auto max-w-96 border border-green-600 rounded mx-4 my-4 text-white">
+            <div className="flex flex-row">
+                <img src={image} alt=""/>
+            </div>
+            <div className="flex flex-row">
+                <div className="flex flex-col">
+                <div className="py-2">Repo: <a href={repoFrontend}>{repoFrontend}</a></div>
+                <div className="">
+                    {
+                        repoBackend &&
+                        <div className="">Repo Backend: <a href={repoBackend}>{repoBackend}</a></div>
+                    }
+                </div>
+                <div className="py-2">
+                {
+                        email && password &&
+                        <div className="">Credentials: {email}, {password}</div>
+                    }
+                </div>
+                <div className="py-2">
+                    <span className="bg-red-700 rounded px-2 mr-2">Live:</span> 
+                    <a href={url}>{url}</a></div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -22,8 +22,22 @@ const Portfolio = () => {
     }, [])
 
     return (
-        <div className="min-w-screen max-w-auto min-h-screen max-height-auto">
-
+        <div className="min-w-screen max-w-auto min-h-screen max-height-auto bg-black">
+            {
+                projects &&
+                projects.map((project) => {
+                    return <Project
+                    key={project.id}
+                    name={project.name}
+                    image={project.image}
+                    repoFrontend={project.repoFrontend}
+                    repoBackend={project.repoBackend}
+                    email={project.email}
+                    password={project.password}
+                    url={project.url}
+                    />
+                })    
+            }
         </div>
     )
 }
