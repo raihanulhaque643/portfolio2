55ab104d8af9a27a25186ad5204f9ee9e9466e1b
Raihanul Haque <raihan.hd.500@gmail.com>
2021-04-17 15:12:34 +0600

    react-spring animations added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import me from '../assets/me.jpg'
 import Typewriter from 'typewriter-effect'
+import {useSpring, animated} from 'react-spring'
 
 const Home = () => {
+
+    const props = useSpring({ 
+        opacity: 1,
+        marginRight: 0,
+        from: { opacity: 0, marginRight: -1000 },
+    })
+
     return (
         <div className="flex flex-row min-w-screen max-w-auto justify-center align-center min-h-screen max-height-auto bg-black">
             <div className="flex flex-col w-full items-center justify-center bg-green-200">
@@ -25,7 +33,9 @@ const Home = () => {
                 </div>
             </div>
             <div className="flex flex-col w-full items-center justify-center">
+                <animated.div style={props}>
                 <img className="rounded-full" src={me} alt="Pic not found"/>
+                </animated.div>
             </div>
         </div>
     )
